be1a888a22f8fee1de9a4b4b8f52edec460f4cad
yeo-seoyun <yeosummer94@gmail.com>
2024-01-22 14:14:52 +0900

    리엑트 컴포넌트 배우기 전 알아둘 것

---
+++ src/lecture/main.jsx
@@ -0,0 +1,33 @@
+import React from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom";
+
+const data = {
+  greetingMessage: ["hello!", "react."],
+  message:
+    "리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.",
+};
+
+const createApp = (data) => {
+  return (
+    <div id="app">
+      <h1>
+        {data.greetingMessage[0].toUpperCase()}
+        {/* 해킹이 아니라, 명시적으로 JSX 구문에서 공백을 설정하는 방법 */}{" "}
+        {/* <br /> */}
+        {data.greetingMessage[1].toUpperCase()}
+      </h1>
+      <p>{data.message}</p>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const reactDomRoot = createRoot(rootElement);
+
+reactDomRoot.render(createApp(data));
+
+// function sum(x, options) {
+//   return x + options.y;
+// }
+
+// sum(10, { y: 9 });
