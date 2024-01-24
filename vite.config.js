9a543f8067ca66058e8d1b9cf0416738f8fad307
yeo-seoyun <yeosummer94@gmail.com>
2024-01-24 10:36:51 +0900

    [실습] 수정, 삭제 버튼

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000,
-    open: true,
-  },
-});
+export default defineConfig(
+  /* vite configuration option object */
+  {
+    plugins: [react()],
+    server: {
+      // host: 'localhost', // default: 'localhost'
+      port: 3000, // default: 5173
+      open: true, // default: false
+    },
+  }
+);
