135f3ae3198225dce9d58e09c4d7546e58b191c9
yeo-seoyun <yeosummer94@gmail.com>
2024-02-02 16:16:34 +0900

    [실습] useRef 훅 사용

--- src/lecture/40-3-side-effects.jsx
+++ src/lecture/40-3-side-effects.jsx
@@ -1,4 +1,4 @@
-import { A11yHidden } from '@/components';
+import { A11yHidden, Stack } from '@/components';
 import { useEffect, useState } from 'react';
 
 const API = import.meta.env.VITE_PB_API;
@@ -19,15 +19,17 @@ async function fetchProducts(options) {
 }
 
 function Exercise() {
+  const [isLoading, setIsLoading] = useState(true);
   const [tableContents, setTableContents] = useState([]);
 
   // 1번만 요청
   useEffect(() => {
     const controller = new AbortController();
 
-    fetchProducts({ signal: controller.signal }).then((data) =>
-      setTableContents(data?.items)
-    );
+    fetchProducts({ signal: controller.signal }).then((data) => {
+      setTableContents(data?.items);
+      setIsLoading(false);
+    });
 
     // 신호를 통해 중복된 요청일 경우 웹 요청을 취소(abort)
     // 클린업
@@ -39,13 +41,66 @@ function Exercise() {
 
   const tableContentsLegnth = tableContents?.length;
 
+  if (isLoading) {
+    return <div role="alert">데이터 로딩 중...</div>;
+  }
+
   return (
     <div>
       <h2 className="text-2xl text-indigo-500 mt-7">Exercise</h2>
-
+      <CountUpDown />
       <DataTable contents={tableContents} />
       <DataTableItemCount count={tableContentsLegnth} />
     </div>
+  );
+}
+
+const COUNT_KEY = 'count';
+
+// persist local storage
+
+const getLocalStorageCount = () => {
+  const count = JSON.parse(localStorage.getItem(COUNT_KEY));
+  return count ?? 0;
+};
+
+function CountUpDown() {
+  const [count, setCount] = useState(getLocalStorageCount);
+
+  const handleInc = () => {
+    const nextCount = count + 1;
+    localStorage.setItem(COUNT_KEY, JSON.stringify(nextCount));
+    setCount(nextCount);
+  };
+
+  const handleDec = () => {
+    const nextCount = count - 1;
+    localStorage.setItem(COUNT_KEY, JSON.stringify(nextCount));
+    setCount(nextCount);
+  };
+
+  const buttonStyle = 'px-4 py-1 bg-sky-800 text-white rounded-md';
+
+  return (
+    <Stack gap={6}>
+      <button
+        className={buttonStyle}
+        type="button"
+        onClick={handleDec}
+        aria-label="1 감소"
+      >
+        -
+      </button>
+      <output className="text-2xl font-bold">{count}</output>
+      <button
+        className={buttonStyle}
+        type="button"
+        onClick={handleInc}
+        aria-label="1 증가"
+      >
+        +
+      </button>
+    </Stack>
   );
 }
 
@@ -56,6 +111,11 @@ function DataTable({ contents }) {
   return (
     <table className={tableStyle}>
       <A11yHidden as="caption">표 제목</A11yHidden>
+      <colgroup>
+        <col width="160" />
+        <col width="240" />
+        <col width="100" />
+      </colgroup>
       <thead>
         <tr>
           <th scope="col" className={borderStyle}>
@@ -70,14 +130,13 @@ function DataTable({ contents }) {
         </tr>
       </thead>
       <tbody>
-        {contents &&
-          contents.map((content) => (
-            <tr key={content.id}>
-              <td className={borderStyle}>{content.id}</td>
-              <td className={borderStyle}>{content.title}</td>
-              <td className={borderStyle}>{content.color}</td>
-            </tr>
-          ))}
+        {contents?.map((content) => (
+          <tr key={content.id}>
+            <td className={borderStyle}>{content.id}</td>
+            <td className={borderStyle}>{content.title}</td>
+            <td className={borderStyle}>{content.color}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
