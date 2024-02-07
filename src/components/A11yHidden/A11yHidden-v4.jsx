0258fbbb3eb3db8de41d3dd70e1aff191972c91a
yeo-seoyun <yeosummer94@gmail.com>
2024-02-07 11:39:00 +0900

    [원론] 컴포넌트 속성 타입 검사

---
+++ src/components/A11yHidden/A11yHidden-v4.jsx
@@ -0,0 +1,24 @@
+// 내보낸 모듈이 없다.
+// import './A11yHidden.css';
+
+// 내보낸 모듈이 있다면?
+import styles from './A11yHidden.module.css';
+import PropTypes from 'prop-types';
+
+function A11yHidden({ as: ComponentName = 'span', ...restProps }) {
+  return (
+    <ComponentName
+      className={styles.group}
+      style={{
+        fontSize: 100,
+      }}
+      {...restProps}
+    />
+  );
+}
+
+A11yHidden.propTypes = {
+  as: PropTypes.oneOfType([PropTypes.string, PropTypes.elementType]),
+};
+
+export default A11yHidden;
--- src/components/CatCardList/CatCardList.jsx
+++ src/components/CatCardList/CatCardList.jsx
@@ -3,18 +3,26 @@ import classes from './CatCardList.module.css';
 
 // 데이터 가져오기
 import catsData from '../../data/cats.json';
+import { CatListType } from './type';
 
-function CatCardList() {
+function CatCardList({ catList }) {
   // 데이터 순환하여 리스트 렌더링
-  const renderCatsList = catsData.map((cat) => {
-    console.log(cat);
-    return <CatCardItem key={cat.id} cat={cat} />;
-  });
+  const renderCatsList =
+    catList ??
+    catsData.map((cat) => {
+      // console.log(cat);
+      return <CatCardItem key={cat.id} cat={cat} />;
+    });
+
   return (
     <section className={classes.component} aria-label="사랑스런 고양이 가족">
       {renderCatsList}
     </section>
   );
 }
+
+CatCardList.propTypes = {
+  catList: CatListType,
+};
 
 export default CatCardList;
